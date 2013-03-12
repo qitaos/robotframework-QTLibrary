4d7c41864cf0a988dd6738e2407e843cfcd8f97e
qitaos <qitaos@gmail.com>
2013-03-12 15:01:46 +0800

    Update __init__.py
    add keyword class

--- src/QTLibrary/keywords/__init__.py
+++ src/QTLibrary/keywords/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: cp936 -*-
+from _logging import _LoggingKeywords
+from _runonfailure import _RunOnFailureKeywords
 from _element import _ElementKeywords
 
 
 __all__ = [
-    "_ElementKeywords"    
+    "_LoggingKeywords",
+    "_RunOnFailureKeywords",
+    "_ElementKeywords"
 ]
-
 
 
 
