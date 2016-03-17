bf899cd185f799bbef65173003c5d47f21d93ede
Jacky Qi <qitaos@gmail.com>
2016-03-17 14:32:50 +0800

    更新readme
    以后可以pip安装了。

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ QTLibrary是一个个人学习研究时做的测试库，参考了一下Selenium
 其中做了几个比较常用的关键字，比如随机生成生日，随机生成身份证号，随机生成中文姓名等。
 
 如果不知道关键字如何使用，请查看一下test目录里的案例。
+
+安装方法：
+
+>pip install robotframework-qtlibrary
+
+Install way:
+
+>pip install robotframework-qtlibrary
+
--- setup.py
+++ setup.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python
 
-#  Copyright (c) 2010 Franz Allan Valencia See
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
-
-"""Setup script for Robot's QTLibrary distributions"""
-
-
-try:
-    from ez_setup import use_setuptools
-    use_setuptools()
-except ImportError:
-    pass
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+import sys
 from os.path import join, dirname
+sys.path.append(join(dirname(__file__), 'src'))
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
 execfile(join(dirname(__file__), 'src', 'QTLibrary', 'version.py'))
 
 DESCRIPTION = """
 QTLibrary is a web testing library for Robot Framework
-that leverages the Selenium 2 (WebDriver) libraries.
+that leverages the QTLibrary libraries.
 """[1:-1]
 
 setup(name         = 'robotframework-qtlibrary',
@@ -42,7 +18,7 @@ setup(name         = 'robotframework-qtlibrary',
       description  = 'QTLibrary for Robot Framework',
       long_description = DESCRIPTION,
       author       = 'Qitao',
-      author_email = 'Qitao016@pingan.com',
+      author_email = 'Qitaos@gmail.com',
       url          = 'https://github.com/qitaos/Robotframework-QTLibrary',
       license      = 'No License',
       platforms    = 'any',
